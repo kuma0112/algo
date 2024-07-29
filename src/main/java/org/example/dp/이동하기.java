2e87d0c467c18137fed3de9aee691e2a0ed4d1ee
kuma0112 <lsj23871@gmail.com>
2024-07-29 23:44:21 +0900

    dp 이동하기 11048 수정

--- src/main/java/org/example/dp/이동하기.java
+++ src/main/java/org/example/dp/이동하기.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 public class 이동하기 {
     static int N, M;
     static int[][] map;
-    static int[][] dp;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,11 +24,15 @@ public class 이동하기 {
 
         for (int i = 1; i <= N ; i++) {
             for (int j = 1; j <= M ; j++) {
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1]);
+                if (i == 1 && j == 1) continue;
+                int max = 0;
+                max = Math.max(max ,map[i-1][j-1]);
+                max = Math.max(max ,map[i-1][j]);
+                max = Math.max(max ,map[i][j-1]);
+                map[i][j] += max;
             }
         }
 
-        System.out.println(dp[N][M]);
+        System.out.println(map[N][M]);
     }
 }
