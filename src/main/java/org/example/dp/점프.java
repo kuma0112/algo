a2ba570dd4de58733daba3634abaa7a42f32851f
kuma0112 <lsj23871@gmail.com>
2024-08-01 00:55:50 +0900

    dp 점프 1890 완료

--- src/main/java/org/example/dp/점프.java
+++ src/main/java/org/example/dp/점프.java
@@ -6,49 +6,38 @@ import java.util.StringTokenizer;
 
 public class 점프 {
     static int N;
-    static int[][] map;
-    static int[][] visited;
-    static int count;
+    static int[][] arr;
+    static long[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N + 1][N + 1];
-        visited = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+        arr = new int[N][N];
+        dp = new long[N][N];
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        solution(1, 1);
-        System.out.println(count);
+        dp[0][0] = 1;
+        solution();
+        System.out.println(dp[N-1][N-1]);
     }
 
-    static void solution(int y, int x) {
-        if (y == N && x == N) {
-            count++;
-            return;
-        }
-
-        int distance = map[y][x]; // 뛸 수 있는 거리
-
-        if (distance == 0) return;
-
-        int ny = y + distance;
-        int nx = x + distance;
-
-        if (ny <= N && visited[ny][x] == 0) {
-            visited[ny][x] = 1;
-            solution(ny, x);
-            visited[ny][x] = 0;
-        }
-
-        if (nx <= N && visited[y][nx] == 0) {
-            visited[y][nx] = 1;
-            solution(y, nx);
-            visited[y][nx] = 0;
+    static void solution() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (dp[i][j] > 0 && arr[i][j] > 0) {
+                    int jump = arr[i][j];
+                    if (j + jump < N) {
+                        dp[i][j + jump] += dp[i][j];
+                    }
+                    if (i + jump < N) {
+                        dp[i + jump][j] += dp[i][j];
+                    }
+                }
+            }
         }
     }
 }
