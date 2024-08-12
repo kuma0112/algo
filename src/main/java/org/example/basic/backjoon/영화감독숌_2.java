e0b5e04b45fae61db34e3721163eb6e1481eff4f
kuma0112 <lsj23871@gmail.com>
2024-08-12 21:37:55 +0900

    영화감독슘

---
+++ src/main/java/org/example/basic/backjoon/영화감독숌_2.java
@@ -0,0 +1,19 @@
+package org.example.basic.backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 영화감독숌_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 1;
+        int num = 666;
+
+        while (count != n) {
+            num++;
+            if (String.valueOf(num).contains("666"))count++;
+        }
+        System.out.println(num);
+    }
+}
