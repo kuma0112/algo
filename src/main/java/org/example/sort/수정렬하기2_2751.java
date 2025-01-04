a1836c79007234fe78de6edcb59071040a6e00bf
kuma0112 <lsj23871@gmail.com>
2025-01-04 21:30:00 +0900

    정렬 수정렬하기2 선택정렬

--- src/main/java/org/example/sort/수정렬하기2_2751.java
+++ src/main/java/org/example/sort/수정렬하기2_2751.java
@@ -15,30 +15,28 @@ public class 수정렬하기2_2751 {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // 버블 정렬
-        bubbleSort(arr);
+        // 선택정렬
+        selectionSort(arr);
 
         for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
     }
 
-    static void bubbleSort(int[] arr) {
-        boolean swapped;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < arr.length-1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(j, j + 1);
-                    swapped = true;
+    static void selectionSort(int[] arr) {
+        int min;
+        int minIdx;
+        for (int i = 0; i < arr.length; i++) {
+            min = arr[i];
+            minIdx = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIdx = j;
                 }
             }
-            if (!swapped) {
-                break;
-            }
+            swap(i, minIdx);
         }
-
     }
 
     static void swap(int a, int b) {
