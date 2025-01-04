399691f88fcd8a6c28042788497cb520c592bbc5
kuma0112 <lsj23871@gmail.com>
2025-01-04 21:13:56 +0900

    정렬 수정렬하기2 버블정렬

--- src/main/java/org/example/sort/수정렬하기2_2751.java
+++ src/main/java/org/example/sort/수정렬하기2_2751.java
@@ -3,19 +3,47 @@ package org.example.sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class 수정렬하기2_2751 {
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
+
+        // 버블 정렬
+        bubbleSort(arr);
+
         for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
+    }
+
+    static void bubbleSort(int[] arr) {
+        boolean swapped;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+
+    }
+
+    static void swap(int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
