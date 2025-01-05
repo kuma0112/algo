241b91a2b58c095e6617dd9425f3631a12232621
kuma0112 <lsj23871@gmail.com>
2025-01-05 09:04:50 +0900

    정렬 수정렬하기2 병합정렬

--- src/main/java/org/example/sort/수정렬하기2_2751.java
+++ src/main/java/org/example/sort/수정렬하기2_2751.java
@@ -5,37 +5,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 수정렬하기2_2751 {
-    static int[] arr;
+    static int[] arr, temp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         arr = new int[n];
+        temp = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // 삽입정렬
-        insertionSort(arr);
+        // 병합정렬
+        mergeSort(0, n-1);
 
         for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
     }
 
-    static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int num = arr[i];
-            int j = i-1;
-
-            while(j >= 0 && arr[j] > num) {
-                arr[j+1] = arr[j]; // 오른쪽으로 한칸 이동
-                j--;
-            }
-            // 만약 arr[j]가 num보다 크지 않으면
-            // 바로 그 숫자 오른쪽, 즉 arr[j+1]에 num을 삽입
-            arr[j+1] = num;
+    static void mergeSort(int left, int right) {
+        if (left >= right) {
+            return;
         }
+
+        int mid = (left + right) / 2;
+
+        mergeSort(left, mid);
+        mergeSort(mid + 1, right);
+        merge(left, mid, right);
     }
 
+    static void merge(int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                k++;
+                i++;
+            } else {
+                temp[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+
+        while (i <= mid) {
+            temp[k] = arr[i];
+            k++;
+            i++;
+        }
+
+        while (j <= right) {
+            temp[k] = arr[j];
+            k++;
+            j++;
+        }
+
+        for (int l = 0; l < j; l++) {
+            arr[l] = temp[l];
+        }
+    }
 }
