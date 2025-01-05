d53a1208904fe0ce952d15632db528861c0fa164
kuma0112 <lsj23871@gmail.com>
2025-01-05 09:39:07 +0900

    정렬 수정렬하기2 퀵정렬 pivot은 맨 오른쪽

--- src/main/java/org/example/sort/수정렬하기2_2751.java
+++ src/main/java/org/example/sort/수정렬하기2_2751.java
@@ -5,68 +5,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 수정렬하기2_2751 {
-    static int[] arr, temp;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         arr = new int[n];
-        temp = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // 병합정렬
-        mergeSort(0, n-1);
+        // 퀵정렬
+        quickSort(0, n-1);
 
         for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
     }
 
-    static void mergeSort(int left, int right) {
+    static void quickSort(int left, int right) {
         if (left >= right) {
             return;
         }
 
-        int mid = (left + right) / 2;
+        int pivot = partition(left, right);
 
-        mergeSort(left, mid);
-        mergeSort(mid + 1, right);
-        merge(left, mid, right);
+        quickSort(left, pivot-1);
+        quickSort(pivot+1, right);
     }
 
-    static void merge(int left, int mid, int right) {
-        int i = left;
-        int j = mid + 1;
-        int k = left;
+    static int partition(int left, int right) {
+        int pivot = arr[right];
+        int i = left -1; // 피벗보다 작은 요소를 위한 가상 인덱스?
 
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp[k] = arr[i];
-                k++;
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
                 i++;
-            } else {
-                temp[k] = arr[j];
-                k++;
-                j++;
+                swap(i, j);
             }
         }
+        swap(i+1, right);
+        return i+1;
+    }
 
-        while (i <= mid) {
-            temp[k] = arr[i];
-            k++;
-            i++;
-        }
-
-        while (j <= right) {
-            temp[k] = arr[j];
-            k++;
-            j++;
-        }
-
-        for (int l = 0; l < j; l++) {
-            arr[l] = temp[l];
-        }
+    static void swap(int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
